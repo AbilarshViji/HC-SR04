566569bf373471019a4f140d4b0ebb8a4ef99695
Abilarsh <vabilarsh@hotmail.ca>
2016-12-12 09:53:23 -0500

    here u go isaac

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -9,7 +9,7 @@ int times[] = {3000, 4000, 5000};
 int distance[] = {20, 10, 5};
 long starttime;
 long endtime;
-NewPing sonar(trigger, echo,500);
+NewPing sonar(trigger, echo,150);
 int range;
 void setup() {
   // put your setup code here, to run once:
@@ -22,21 +22,21 @@ void setup() {
 //http://forum.arduino.cc/index.php?topic=45787.0
 void loop() {
  // if (button == HIGH) {
-      delay(100);
+      delay(50);
       range = sonar.ping_cm();
       Serial.println(range);
  //   while ((endtime - starttime) <= times[1]) //wont work
-      if (range == distance[1]) {
-        digitalWrite(left_motor, HIGH);
-        digitalWrite(right_motor, HIGH);
+      if (range == distance[0]) {
+        digitalWrite(left_motor, 250);
+        digitalWrite(right_motor, 255);
       }
-      else if (range > distance[1]) {
-        analogWrite(right_motor, 127);
-        analogWrite(left_motor, 255);
+      else if (range > distance[0]) {
+        analogWrite(right_motor, 110);
+        analogWrite(left_motor, 220);
 
       }
-      else if(range < distance[1]) {
-        analogWrite(left_motor, 127);
+      else if(range < distance[0]) {
+        analogWrite(left_motor, 110);
         analogWrite(right_motor, 255);
 
       }
