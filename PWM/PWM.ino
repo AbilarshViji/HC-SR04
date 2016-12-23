af03fb8c43947be510721bf04d50eccbd86bf183
Abilarsh <vabilarsh@hotmail.ca>
2016-12-23 09:45:17 -0500

    FINISHED
    probably

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -8,9 +8,9 @@ int button = 13;
 int times[] = {3000, 4000, 5000};
 int distance[] = {30, 10, 5};
 double pingpong = 0;
-int starter;
-NewPing sonar(trigger, echo, 150);
-double range;
+bool starter;
+NewPing sonar(trigger, echo, 200);
+int range;
 void setup() {
   // put your setup code here, to run once:
   pinMode(led, OUTPUT);
@@ -18,38 +18,36 @@ void setup() {
   pinMode(right_motor, OUTPUT);
   pinMode(button, INPUT);
   Serial.begin(9600);
+  // sets initial motors to 0
+  analogWrite(left_motor, 0);
+  analogWrite(right_motor, 0);
 }
 //http://forum.arduino.cc/index.php?topic=45787.0
 void loop() {
-  if (digitalRead(button) == HIGH) {
-    starter = 1;
+  //  Serial.println(starter);
+
+  if (digitalRead(button) == 1) {
+    starter += 1;
   }
-  while (starter==1) {
-//    delay(50);
-//    range = sonar.ping_cm();
-//    Serial.println(range);
-//    pingpong = 0;
-//    for (int i = 0; i <= 24; i++) {
-//      pingpong += sonar.ping();
-//      delay(10);
-//    }
-//    range = pingpong / 25 / 58.2;
-range = sonar.ping_cm();
-    Serial.println(range); 
-    //   while ((endtime - starttime) <= times[1]) //wont work
+  while (starter >= 1 ) {
+  //    Serial.println(starter);
+//Serial.print("esr");
+    //  delay(50);
+    range = sonar.ping_cm();
+    Serial.println(range);
+    analogWrite(left_motor, 255);
+    analogWrite(right_motor, 255);
     if (range == distance[0]) {
       analogWrite(left_motor, 255);
       analogWrite(right_motor, 255);
-    }
-    else if (range > distance[0]) {
-      analogWrite(right_motor, 150);
+      }
+      else if (range > distance[0]) {
+      analogWrite(right_motor, 127);
       analogWrite(left_motor, 190);
-
-    }
-    else if (range < distance[0]) {
+      }
+      else if (range < distance[0]) {
       analogWrite(left_motor, 80);
       analogWrite(right_motor, 255);
-    }
+      }
   }
 }
-
