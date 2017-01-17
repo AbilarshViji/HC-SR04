765e41979c20e77613687bc9627b9f8df421aef1
Abilarsh <vabilarsh@hotmail.ca>
2017-01-17 15:32:59 -0500

    simpletimer lib

--- PWM/PWM.ino
+++ PWM/PWM.ino
@@ -1,20 +1,18 @@
 #include <NewPing.h>
-#include "MillisTimer.h"
+#include <SimpleTimer.h>
 
 int trigger = 4;
 int echo = 3;
 int left_motor = 10;
 int right_motor = 9;
 int button = 13;
+int starter = 0;
 int range;
 NewPing sonar(trigger, echo, 200);
-MillisTimer timer1 = MillisTimer(1000);
-
+SimpleTimer timer;
 
 void setup() {
-  timer1.setInterval(1000);
-  timer1.setRepeats(1);
-  timer1.start();
+
   pinMode(left_motor, OUTPUT);
   pinMode(right_motor, OUTPUT);
   pinMode(button, INPUT);
@@ -23,24 +21,31 @@ void setup() {
   analogWrite(left_motor, 0);
   analogWrite(right_motor, 0);
 }
+
+void circle() {
+  analogWrite(left_motor, 0);
+  analogWrite(right_motor, 255);
+  delay(4000);
+}
+
 void abitech(int distance) {
   range = sonar.ping_cm();
   Serial.println(range);
-    if (range == distance) {
-      analogWrite(left_motor, 255);
-      analogWrite(right_motor, 255);
-timer1.run();
-      }
-    }
+  if (range == distance) {
+    analogWrite(left_motor, 255);
+    analogWrite(right_motor, 255);
+    timer.setTimeout(2000, circle);
+    timer.run();
+  }
 
-    else if (range > distance) {
-      analogWrite(right_motor, 127);
-      analogWrite(left_motor, 190);
-    }
+  else if (range > distance) {
+    analogWrite(right_motor, 127);
+    analogWrite(left_motor, 190);
+  }
 
-    else if (range < distance) {
-      analogWrite(left_motor, 80);
-      analogWrite(right_motor, 255);
+  else if (range < distance) {
+    analogWrite(left_motor, 80);
+    analogWrite(right_motor, 255);
   }
 }
 
@@ -49,6 +54,6 @@ void loop() {
     starter += 1;
   }
   while (starter >= 1 ) {
-abitech(20);
+    abitech(20);
   }
 }
